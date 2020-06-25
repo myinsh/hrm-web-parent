99add0c3e336be066a017b562533f599095c5cc6
yinsh <765009618@qq.com>
2020-06-25 13:23:33 +0800

    安装live-server&nginx配置文件

--- hrm-web-plat/config/index.js
+++ hrm-web-plat/config/index.js
@@ -7,11 +7,12 @@ module.exports = {
     index: path.resolve(__dirname, '../dist/index.html'),
     assetsRoot: path.resolve(__dirname, '../dist'),
     assetsSubDirectory: 'static',
-    assetsPublicPath: '/vue-admin/',
+    assetsPublicPath: '/',
     productionSourceMap: true,
     // Gzip off by default as many popular static hosts such as
     // Surge or Netlify already gzip all static assets for you.
-    // Before setting to `true`, make sure to:
+    // Before setting to `true`, make sure
+      // to:
     // npm install --save-dev compression-webpack-plugin
     productionGzip: false,
     productionGzipExtensions: ['js', 'css'],
